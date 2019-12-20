8548b60335fe4a39bbc3119c60f42121493aa115
marlonchiu <Knight1898@outlook.com>
2019-12-20 13:46:17 +0800

    init project

--- python-liaoxuefeng/samples/01_basic/do_input.py
+++ python-liaoxuefeng/samples/01_basic/do_input.py
@@ -1,3 +1,13 @@
-name = input()
+# name = input('please enter your name:')
 
-print('Hello,', name)
+# print('Hello,', name)
+
+# print absolute value an integer:
+# int 是将 input 输出的 str 类型转化为数字
+
+b = input('请输入数字：')
+a = int(b)
+if a >= 100:
+    print(a)
+else:
+    print(-a)
